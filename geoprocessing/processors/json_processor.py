50274946f5170931f41d34d31bc2aeba2e70a17f
UmairMirza1 <umair3215@gmail.com>
2023-12-20 00:55:08 +0500

    Added config and calculation files

---
+++ geoprocessing/processors/json_processor.py
@@ -0,0 +1,13 @@
+import json
+
+def read_config_json(file_path):
+    """
+    Read configuration parameters from a JSON file.
+    Args:
+    file_path: Path to the JSON configuration file.
+    Returns:
+    A dictionary containing configuration parameters.
+    """
+    with open(file_path, 'r') as file:
+        config = json.load(file)
+    return config
