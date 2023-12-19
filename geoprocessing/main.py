72f2dd3018e01f0f87c369fac2fe54523038362a
UmairMirza1 <umair3215@gmail.com>
2023-12-20 01:04:12 +0500

    smoke test fixes

--- geoprocessing/main.py
+++ geoprocessing/main.py
@@ -2,12 +2,12 @@ import pandas as pd
 import geopandas as gp
 import json
 from shapely.geometry import Point
-from json_parser import read_config
+from processors.json_processor import read_config_json
 from constants.crop_dict import color_id, crop_name
 def run():
     print("Reading data...")
 
 if __name__ == "__main__":
-    config = read_config("config.json")
+    config = read_config_json("config.json")
     print(config)
     print(color_id)
