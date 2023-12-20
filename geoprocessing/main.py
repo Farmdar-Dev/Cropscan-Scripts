f48b0f69e03a2e10a6780ac3b3144303d2f5f166
UmairMirza1 <umair3215@gmail.com>
2023-12-20 15:39:09 +0500

    Intersection baseline working

--- geoprocessing/main.py
+++ geoprocessing/main.py
@@ -1,13 +1,17 @@
 from processors.json_processor import read_config_json
-from constants.crop_dict import color_id, crop_name
+#from constants.crop_dict import color_id, crop_name
 from processors.shp_proccesor import process_shapefiles
-from intersection.intersect import intersect
+from intersection.intersect import intersect_caller
 def run():
     print("Reading data...")
+
+
 
 if __name__ == "__main__":
     config = read_config_json("config.json")
     dataframes_by_crop = process_shapefiles(config["shapefile_paths"], config["unit"])
-    print(dataframes_by_crop)
-    #intersected_dataframes = intersect(dataframes_by_crop, config["boundary_file_path"])
-    
+    #intersected_dataframes = intersect_caller(dataframes_by_crop, config["boundary_file_paths"])
+    for dataframe in dataframes_by_crop:
+        interesected_df = intersect_caller(dataframe, config["boundary_file_paths"])
+        print( "INTERSECTED DATAFRAME")
+        print(interesected_df)
