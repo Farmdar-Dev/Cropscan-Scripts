070bed7b49ee5f032f6df8eb2cb7b24e99ba732e
UmairMirza1 <umair3215@gmail.com>
2023-12-21 22:55:18 +0500

    Added file for mapbox working

---
+++ geoprocessing/tilesets.py
@@ -0,0 +1,12 @@
+# Move all the mapbox logic here using this shp processor 
+
+from processors.shp_proccesor import process_shapefiles
+
+
+def smoke_test():
+    print("Smoke test passed!")
+
+
+if __name__ == "__main__":
+    smoke_test()
+    
