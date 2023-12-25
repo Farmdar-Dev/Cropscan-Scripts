4ab22a96fd869d1582d44848f64b68acb947d8b2
UmairMirza1 <umair3215@gmail.com>
2023-12-25 21:23:32 +0500

    modified logic after merge

--- geoprocessing/main.py
+++ geoprocessing/main.py
@@ -2,7 +2,7 @@ from processors.json_processor import read_config_json , survey_json_creator
 # from constants.crop_dict import color_id, crop_name
 from processors.shp_proccesor import process_shapefiles
 from intersection.intersect import intersect_all
-
+from processors.dataframe_processor import split_dfs_by_predicted
 
 def run():
     print("Reading data...")
@@ -11,7 +11,8 @@ def run():
 if __name__ == "__main__":
     config = read_config_json("config.json")
     unit = config["unit"]
-    dataframes_by_crop = process_shapefiles(config["shapefile_paths"], unit)
+    shapefiles = process_shapefiles(config["shapefile_paths"])
+    dataframes_by_crop = split_dfs_by_predicted(shapefiles)
     intersected_dataframes = intersect_all(
         dataframes_by_crop, config["boundary_details"], "output", unit, config["esurvey_path"])
     for df in intersected_dataframes:
