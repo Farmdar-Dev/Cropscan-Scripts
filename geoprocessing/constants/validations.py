91c0be00671195bfa5fe51ee84e7d6be622a6920
UmairMirza1 <umair3215@gmail.com>
2024-01-06 16:12:22 +0500

    Validation constants added

---
+++ geoprocessing/constants/validations.py
@@ -0,0 +1,19 @@
+report_type_names = [
+    "Yield Estimation",
+    "Plant Health",
+    "Crop Scan",
+    "Sowing Date",
+    "Variety Scan",
+    "Plant Stress",
+    "Harvest Monitoring",
+    "VRA",
+    "SOM",
+    "Nitrogen Report",
+    "Water Stress"
+]
+
+crop_names = [
+    "Urban", "Cotton", "Maize", "Others", "Orchards", "Juaar", "Rice",
+    "Other Vegetation", "Chillis", "Musturd", "Canola", "Banana", "Tobacco",
+    "Wheat", "Tomato", "Mountain", "Sugarcane", "Watermelon", "Grapes", "Potato"
+]
