cfd29c683795dfeb40fccb781558bd0de39cdfd0
Hiba-N <hiba.nasir@farmdar.co.uk>
2024-05-27 20:44:12 +0500

    We have officially fixed area forever

--- geoprocessing/utils/get_default_crs.py
+++ geoprocessing/utils/get_default_crs.py
@@ -1,12 +1,13 @@
 import geopandas as gpd
+from pyproj import CRS
 
 def get_crs_string(file_path):
     gdf = gpd.read_file(file_path)
     utm_crs = gdf.estimate_utm_crs()
     if utm_crs:
         crs_str = utm_crs.to_string()
-        if any(zone in crs_str for zone in ['UTM zone 41', 'UTM zone 42', 'UTM zone 43']):
-            return "EPSG:32642"
+        if any(zone in crs_str for zone in ['EPSG:32641', 'EPSG:32642', 'EPSG:32643']):
+            return CRS.from_epsg("32642")
         else:
             return crs_str
     return "CRS could not be estimated"
